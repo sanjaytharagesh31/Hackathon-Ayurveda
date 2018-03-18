2f1af8dfe8e55f43a528925ce057ea91b2f929c3
sanjaytharagesh <sanjay311999@gmail.com>
2018-03-18 18:01:43 +0530

    app source code

---
+++ FirebaseWork/app/src/main/java/ericjoseph/com/firebasework/Upload.java
@@ -0,0 +1,28 @@
+package ericjoseph.com.firebasework;
+
+/**
+ * Created by Belal on 8/25/2017.
+ */
+public class Upload {
+
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
