8b19d47a2c07de59ca5b26135231bfd8b71ee2bd
karan101099 <32812084+karan101099@users.noreply.github.com>
2018-03-20 15:27:03 +0530

    added weight units

--- Ayur Bot/ayur.py
+++ Ayur Bot/ayur.py
@@ -7,6 +7,26 @@ def convert(unit, input):
         "hasta": input*45.72,
         "nrpahasta": input*55.88,
         "rajahasta": input*55.88,
-        "vyama": input*182.88
+        "vyama": input*182.88,
+        //end of length units
+        "karsa": input*12,
+        "tola": input*12,
+        "masa": input,
+        "ratti": input*125,
+        "gunga": input*125,
+        "palam": input*48,
+        "prasrti": input*96,
+        "kudava": input*192,
+        "manika": input*384,
+        "prastha": input*768,
+        "adhaka": input*3073,
+        "drona": input*12228,
+        "surpa": input*24576,
+        "droni": input*49152,
+        "vahi": input*49152,
+        "khari": input*196608,
+        "tula": input*4800,
+        "bhara": input*96,
+        //end of weight units
     }
     switcher.get(unit, "Unknown unit");
