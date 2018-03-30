c8e935d04abe2715a2cab4a38cdac832fdeda681
sanjay311999 <sanjay311999@gmail.com>
2018-03-31 03:49:50 +0530

    Integrated with Flask
    Courtesy, @siddharth_sham

--- Ayur Website/main.py
+++ Ayur Website/main.py
@@ -9,5 +9,13 @@ Compress(app)
 def master():
     return render_template('index.html')
 
+@app.route('/siddha',methods=['POST','GET'])
+def siddha():
+    return render_template('siddha.html')
+
+@app.route('/unani',methods=['POST','GET'])
+def unani():
+    return render_template('unani.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
